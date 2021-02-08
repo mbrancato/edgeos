a8af9143aa82ddece27089bf2f543f473c97d2db
Mike Brancato <mike@mikebrancato.com>
2021-02-07 21:47:02 -0500

    Breakup structs to get past compiler issue

--- scripts/parseconfig.sh
+++ scripts/parseconfig.sh
@@ -5,40 +5,40 @@ getfieldtype() {
     TYPE=$(cat "$1/node.def" | grep "^type" | cut -d ";" -f 1 | cut -d ":" -f 2 | sed 's/[[:blank:]]//g')
     case $TYPE in
     "txt")
-      echo "*string \`json:\"$2,omitempty\"\`"
+      echo "string \`json:\"$2,omitempty\"\`"
       ;;
     "bool")
-      echo "*bool \`json:\"$2,omitempty\"\`"
+      echo "bool \`json:\"$2,omitempty\"\`"
       ;;
     "u32")
-      echo "*int \`json:\"$2,omitempty\"\`"
+      echo "int \`json:\"$2,omitempty\"\`"
       ;;
     "ipv4")
-      echo "*IPv4 \`json:\"$2,omitempty\"\`"
+      echo "IPv4 \`json:\"$2,omitempty\"\`"
       ;;
     "ipv6")
-      echo "*IPv6 \`json:\"$2,omitempty\"\`"
+      echo "IPv6 \`json:\"$2,omitempty\"\`"
       ;;
     "ipv4,ipv6")
-      echo "*IP \`json:\"$2,omitempty\"\`"
+      echo "IP \`json:\"$2,omitempty\"\`"
       ;;
     "ipv4net")
-      echo "*IPv4Net \`json:\"$2,omitempty\"\`"
+      echo "IPv4Net \`json:\"$2,omitempty\"\`"
       ;;
     "ipv6net")
-      echo "*IPv6Net \`json:\"$2,omitempty\"\`"
+      echo "IPv6Net \`json:\"$2,omitempty\"\`"
       ;;
     "ipv4net,ipv6net")
-      echo "*IPNet \`json:\"$2,omitempty\"\`"
+      echo "IPNet \`json:\"$2,omitempty\"\`"
       ;;
     "ipv6,ipv6net")
-      echo "*IPv6Net \`json:\"$2,omitempty\"\`"
+      echo "IPv6Net \`json:\"$2,omitempty\"\`"
       ;;
     "macaddr")
-      echo "*MacAddr \`json:\"$2,omitempty\"\`"
+      echo "MacAddr \`json:\"$2,omitempty\"\`"
       ;;
     *)
-      echo "*string \`json:\"$2,omitempty\"\`"
+      echo "json.RawMessage \`json:\"$2,omitempty\"\`"
       ;;
     esac
   else
@@ -46,8 +46,16 @@ getfieldtype() {
   fi
 }
 
+# This creates many smaller structs instead of a large config struct to avoid
+# this issue: https://github.com/golang/go/issues/18920
 getnodes() {
   local n
+  local HEAD=$2
+  local TAIL=$3
+  local PREFIX=$4
+  local CHILDREN=()
+
+  echo "$HEAD"
   for i in $(find "$1" -type d -mindepth 1 -maxdepth 1 | awk -F '/' '{print $NF}'); do
     n="$i"
 
@@ -62,9 +70,9 @@ getnodes() {
         FIELD="$FIELD$f"
       done
       FIELD=$(echo "$FIELD" | sed 's/^6/Six/')
-      echo "$FIELD *map[string]struct{"
-      getnodes "$1/$n/node.tag/"
-      echo "} \`json:\"$n,omitempty\"\`"
+      #getnodes "$1/$n/node.tag/" "$FIELD *map[string]struct{" "} \`json:\"$n,omitempty\"\`"
+      echo "$FIELD *$PREFIX$FIELD \`json:\"$n,omitempty\"\`"
+      CHILDREN+=("$1/$n/node.tag/;type $PREFIX$FIELD map[string]struct{;};$PREFIX$FIELD")
     elif [ $(find "$1/$n/" -mindepth 1 -maxdepth 1 -type d | wc -l) -gt 0 ]; then
       FIELDLIST=${n//-/ }
       FIELDLIST=${FIELDLIST//_/ }
@@ -76,9 +84,9 @@ getnodes() {
         FIELD="$FIELD$f"
       done
       FIELD=$(echo "$FIELD" | sed 's/^6/Six/')
-      echo "$FIELD *struct {"
-      getnodes "$1/$n/"
-      echo "} \`json:\"$n,omitempty\"\`"
+      #getnodes "$1/$n/" "$FIELD *struct {" "} \`json:\"$n,omitempty\"\`"
+      echo "$FIELD *$PREFIX$FIELD \`json:\"$n,omitempty\"\`"
+      CHILDREN+=("$1/$n/;type $PREFIX$FIELD struct{;};$PREFIX$FIELD")
     else
       FIELDLIST=${n//-/ }
       FIELDLIST=${FIELDLIST//_/ }
@@ -93,9 +101,50 @@ getnodes() {
       echo "$FIELD $FIELDSPEC"
     fi
   done
+  echo "$TAIL"
+  printf "\n"
+
+  for child in "${CHILDREN[@]}"; do
+    NEXTPATH=$(echo "$child" | cut -f 1 -d ';' )
+    NEXTHEAD=$(echo "$child" | cut -f 2 -d ';' )
+    NEXTTAIL=$(echo "$child" | cut -f 3 -d ';' )
+    NEXTPREFIX=$(echo "$child" | cut -f 4 -d ';' )
+    getnodes "$NEXTPATH" "$NEXTHEAD" "$NEXTTAIL" "$NEXTPREFIX"
+  done
+
+}
+
+getfirstnodes() {
+  local n
+
+  NODES=()
+
+  for i in $(find "$1" -type d -mindepth 1 -maxdepth 1 | awk -F '/' '{print $NF}'); do
+    n="$i"
+
+    if [ $(find "$1/$n/" -mindepth 1 -maxdepth 1 -type d | wc -l) -gt 0 ]; then
+      FIELDLIST=${n//-/ }
+      FIELDLIST=${FIELDLIST//_/ }
+      FIELDLIST=${FIELDLIST//\./}
+      FIELD=""
+      for f in $FIELDLIST; do
+        f=$(echo "$f" | sed 's/^6/Six/')
+        f="$(tr '[:lower:]' '[:upper:]' <<<${f:0:1})${f:1}"
+        FIELD="$FIELD$f"
+      done
+      FIELD=$(echo "$FIELD" | sed 's/^6/Six/')
+      NODES+=("$FIELD Config$FIELD \`json:\"$n,omitempty\"\`")
+      getnodes "$1/$n/" "type Config$FIELD *struct{" "}" "Config$FIELD"
+    fi
+  done
+
+  echo "type Config struct {"
+  for node in "${NODES[@]}"; do
+    echo "$node"
+  done
+  echo "}"
 }
 
 printf "package sdk\n\n"
-echo "type Config struct {"
-getnodes "$1"
-echo "}"
+
+getfirstnodes "$1"
